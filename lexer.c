639568ba7be2d6aac08f154c3f139041e9d16d72
nagelwy <nagelwyatt@yahoo.com>
2023-06-11 09:01:16 -0400

    INITIAL PUSH

--- lexer.c
+++ lexer.c
@@ -17,6 +17,8 @@ typedef struct token {
 
 FILE *loadFile(char *filename);
 char **getSource(FILE *fp, char **source);
+void printSource(char **source);
+int isNumber(char *string);
 
 int main(int argc, char **argv)
 {
@@ -25,6 +27,8 @@ int main(int argc, char **argv)
 
     char **source = NULL;
     source = getSource(fp, source);
+
+    printSource(source);
 }
 // Function to open file for reading and checks for NULL value
 FILE *loadFile(char *filename)
@@ -41,11 +45,45 @@ char **getSource(FILE *fp, char **source)
     source = malloc(MAX_ARRAY * sizeof(char*));
 
     int buffer;
+    int i = 0;
     do {
+        char *temp = malloc(12 * sizeof(char));
+        fscanf(fp, "%s ", temp);
 
+        if (strlen(temp) > MAX_IDENT) {}
+            // error
+        
+        source[i] = malloc((strlen(temp) + 1) * sizeof(char));
+        strcpy(source[i], temp);
+
+        i++;
+        free(temp);
     } while (!feof(fp));
 
     // Close the file and return the program array
     fclose(fp);
     return source;
+}
+void printSource(char **source)
+{
+    int i = 0;
+    while (source[i] != NULL)
+    {
+        printf("%s\n", source[i]);
+
+        i++;
+    }
+    
+}
+
+// Function to check if string is a number or has other symbols in it
+int isNumber(char *string)
+{
+    int i = strlen(string);
+    while (i--) {
+        if (string[i] > 47 && string[i] < 58)
+            continue;
+        return 0;
+    }
+    return 1;
 }
