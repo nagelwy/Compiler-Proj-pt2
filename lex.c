5ab82b982746b3220ce6da5f606691948634ed54
nagelwy <nagelwyatt@yahoo.com>
2023-06-15 16:30:20 -0400

    Almost Done

--- lex.c
+++ lex.c
@@ -10,11 +10,6 @@
 #define MAX_IDENT 11
 #define MAX_NUM 5
 
-typedef struct token {
-    int num;
-    char *ident;
-} token;
-
 typedef enum {
     skipsym = 1, identsym, numbersym, plussym, minussym,
     multsym, slashsym, xorsym, eqlsym, neqsym, lessym, leqsym,
@@ -25,8 +20,8 @@ typedef enum {
 } token_type;
 
 char *keywords[] = {
-    "const", "var", "procedure", "call", "begin", "end", "if", "then", "xor",
-    "else", "while", "do", "read", "write", NULL
+    "buffer", "const", "var", "procedure", "call", "begin", "end", "if", "then", "xor",
+    "else", "while", "do", "read", "write", "skip", "becomes", NULL
 };
 
 typedef struct Token {
@@ -43,6 +38,7 @@ void printSource(char **source);
 void createTokens(char** source);
 void addToken(char* string);
 
+int checkNumber(int num);
 int getType(Token t);
 
 void printLexemeTable();
@@ -66,33 +62,10 @@ int main(int argc, char **argv)
     char **source = NULL;
     source = getSource(fp, source);FILE *loadFile(char *filename);
 
-	// printSource(source);
-
-	// addToken(numbersym, "test");
-
-	/* Here we can start the switch and go through the array (probably a while loop)
-	 * We check if it's a character, digit, or symbol.
-	 * we check if the characters are a special keyword via the isKeyword function
-	 * use the add token function and add them and then call the print function
-	 *
-	 * if the token is a keyword it will return the number from the isKeyword function,
-	 * if it isn't a keyword the token will be the identsym
-	 * i.e.  int token = isKeyword(lexeme) ? isKeyword(lexeme) : identsym;
-             addToken(token, lexeme);
-	 *
-	 * then we check the numbers and add those to the token
-	 * i.e. addToken(numbersym, lexeme);
-	 *
-	 * then we check the symbols and use a switch statement and add them that way
-	 * i.e.  case '+': addToken(plussym, lexeme); break;
-	*/
-
 	createTokens(source);
 
-	printLexemeTable();
+	// printLexemeTable();
 	printLexemeList();
-
-   // printSource(source);
 }
 // Function to open file for reading and checks for NULL value
 FILE* loadFile(char *filename) {
@@ -113,15 +86,21 @@ FILE* loadFile(char *filename) {
 	printf("\n");
 	return file;
 }
+// Function that scans the source program file
 char** getSource(FILE *fp, char **source) {
 	source = malloc(MAX_ARRAY * sizeof(char*));
 	do {
-		char *temp = malloc(12 * sizeof(char));
+		char temp[50];
 		fscanf(fp, "%s ", temp);
 
-		if (strlen(temp) > MAX_IDENT) {
+		if (strcmp(temp, "/*") == 0)
+		{
+			while(strcmp(temp, "*/") != 0)
+			{
+				fscanf(fp, "%s", temp);
+			}
+			fscanf(fp, "%s", temp);
 		}
-		// error
 
 		source[progLen] = malloc((strlen(temp) + 1) * sizeof(char));
 		strcpy(source[progLen], temp);
@@ -137,14 +116,35 @@ char** getSource(FILE *fp, char **source) {
 
 // Creates the lexeme table and adds each token to it
 void createTokens(char** source) {
+	printf("\nLexeme Table:\n\n");
+    printf("lexeme\t\ttoken type\n");
 	// Loop through the entire source program
 	for (int i = 0; i < progLen; i++) {
+		if (strlen(source[i]) > 12 && isSpecial(source[i][strlen(source[i]) - 1]))
+		{
+			printf("Error : Identifier names cannot exceed 11 characters\n");
+
+			char special[2];
+			special[0] = source[i][strlen(source[i]) - 1];
+			special[1] = '\0';
+			addToken(special);
+
+			continue;
+		}
+		else if (strlen(source[i]) > 11)
+		{
+			printf("Error : Identifier names cannot exceed 11 characters\n");
+			continue;
+		}
+		
 		// Current instruction to be tokenized
 		char curr[12];
 		strcpy(curr, source[i]);
 
+		char temp[3] = {curr[strlen(curr) - 2], curr[strlen(curr) - 1]};
+
 		// If it is a special character
-		if (isSpecial(curr[strlen(curr) - 1]))
+		if (isSpecial(curr[strlen(curr) - 1]) && !isSpecialCase(temp) && strlen(curr) > 1)
 		{
 			char newCurr[strlen(curr)];
 
@@ -155,28 +155,35 @@ void createTokens(char** source) {
 			memset(newCurr, '\0', sizeof(newCurr));
 			newCurr[0] = curr[strlen(curr) - 1];
 			addToken(newCurr);
+
+			continue;
 		}
+		
+		addToken(curr);
 	}
 }
 // Adds a lexeme token to the lexeme table
 void addToken(char* string) {
 	Token t;
-	// Checks if it is identifier
-    if (!isKeyword(string) && !isNumber(string) && !isSpecial(string[0]) && !isSpecialCase(string))
-	{
-		// Check string length
-		if (strlen(string) > 11)
-		{
-			// error
-		}
 
+	// Flag that checks for errors
+	int errorFlag = 0;
+
+	// Checks if it is identifier
+    if (isKeyword(string) == 0 && 
+		!isNumber(string) && 
+		!isSpecial(string[0]) && 
+		!isSpecialCase(string) && 
+		string[0] > 96 &&
+		string[0] < 123)
+	{
 		t.type = identsym;
 		strcpy(t.ident, string);
 		lexemeTable[lexemeCount++] = t;
 	}
-	// Checks if it is operator
+	// Checks if it is an operator
 	else if (isSpecial(string[0]) && !isSpecialCase(string))
-	{
+	{	
 		switch (string[0])
 		{
 			case '+':
@@ -251,87 +258,177 @@ void addToken(char* string) {
 	// Checks if it is a special case
 	else if (isSpecialCase(string))
 	{
-		if (strcmp(string, ":="))
+		switch (string[0])
 		{
-			t.type = becomessym;
-			strcpy(t.ident, string);
-			lexemeTable[lexemeCount++] = t;
+			case ':':
+				t.type = becomessym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case '<':
+				t.type = leqsym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case '>':
+				t.type = geqsym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			default:
+				break;
 		}
-		else if (strcmp(string, "<="))
+		// else if (strcmp(string, "<>"))
+		// {
+		// 	t.type = xorsym;
+		// 	strcpy(t.ident, string);
+		// 	lexemeTable[lexemeCount++] = t;
+		// }
+	}
+	else if (isKeyword(string) > 0)
+	{
+		switch (isKeyword(string))
 		{
-			t.type = leqsym;
-			strcpy(t.ident, string);
-			lexemeTable[lexemeCount++] = t;
-		}
-		else if (strcmp(string, ">="))
-		{
-			t.type = geqsym;
-			strcpy(t.ident, string);
-			lexemeTable[lexemeCount++] = t;
-		}
-		else if (strcmp(string, "<>"))
-		{
-			t.type = xorsym;
-			strcpy(t.ident, string);
-			lexemeTable[lexemeCount++] = t;
+			case 1:
+				t.type = constsym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 2:
+				t.type = varsym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 3:
+				t.type = procsym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 4:
+				t.type = callsym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 5:
+				t.type = beginsym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 6:
+				t.type = endsym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 7:
+				t.type = ifsym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 8:
+				t.type = thensym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 9:
+				t.type = xorsym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 10:
+				t.type = elsesym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 11:
+				t.type = whilesym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 12:
+				t.type = dosym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 13:
+				t.type = readsym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 14:
+				t.type = writesym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 15:
+				t.type = skipsym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			case 16:
+				t.type = becomessym;
+				strcpy(t.ident, string);
+				lexemeTable[lexemeCount++] = t;
+				break;
+			default:
+				break;
 		}
 	}
-	// else if ()
-	// {
+	else if (isNumber(string) > 0)
+	{
+		if (checkNumber(isNumber(string)) > 5) {
+			printf("Error : Numbers cannot exceed 5 digits\n");
+			errorFlag++;
+		} else {
+			t.type = numbersym;
+			t.num = isNumber(string);
+			lexemeTable[lexemeCount++] = t;
+		} 
+	}
+	else
+	{
+		printf("Error : Invalid Symbol\n");
+		errorFlag++;
+	}
 
-	// }
+	// Prints the token depending on token type
+	if (!errorFlag)
+	{
+		if (t.type != numbersym)
+			printf("%s\t\t%d\n", t.ident, t.type);
+		else 
+			printf("%d\t\t%d\n", t.num, t.type);
+	}
+}
+int checkNumber(int num) {
+	int i = 0;
+	for (i = 0; num != 0; i++, num = num/10) {}
+	return i;
 }
 // Checks if token type is a string or number
 int getType(Token t) {
-	if (t.type == skipsym ||
-		t.type == identsym ||
+	if ((t.type == constsym ||
+		t.type == varsym ||
+		t.type == procsym ||
+		t.type == callsym ||
 		t.type == beginsym ||
 		t.type == endsym ||
 		t.type == ifsym ||
 		t.type == thensym ||
+		t.type == xorsym ||
+		t.type == elsesym ||
 		t.type == whilesym ||
 		t.type == dosym ||
 		t.type == readsym ||
 		t.type == writesym ||
-		isSpecial(t.ident[0]))
+		t.type == skipsym ||
+		t.type == becomessym ||
+		t.type == identsym ||
+		isSpecial(t.ident[0])) &&
+		t.type != numbersym)
 		return 1;
 	else
 		return 0;
 }
-
-// Prints the lexem table
-void printLexemeTable() {
-    printf("Lexeme Table:\n\n");
-    printf("lexeme\t\ttoken type\n");
-    printf("------------------------\n");
-    for (int i = 0; i < lexemeCount; i++) {
-		// Checks if lexeme is number or word
-        if (getType(lexemeTable[i]))
-			printf("%s\t\t%d\n", lexemeTable[i].ident, lexemeTable[i].type);
-		else
-			printf("%d\t\t%d\n", lexemeTable[i].num, lexemeTable[i].type);
-    }
-    printf("\n");
-}
-// Prints the lexeme list
-void printLexemeList() {
-    printf("Lexeme List:\n");
-    for (int i = 0; i < lexemeCount; i++) {
-		// Checks if to print identifier or not
-		if (lexemeTable[i].type == identsym)
-			printf("%d %s ", lexemeTable[i].type, lexemeTable[i].ident);
-		else
-			printf("%d ", lexemeTable[i].type);
-	}
-    printf("\n");
-}
-
-
-void printSource(char **source) {
-	for (int i = 0; i < progLen; i++)
-		printf("%s\n", source[i]);
-}
-
 // Function to check if string is a number or has other symbols in it
 int isNumber(char *string) {
 	int i = strlen(string);
@@ -340,46 +437,14 @@ int isNumber(char *string) {
 			continue;
 		return 0;
 	}
-	return 1;
+	return atoi(string);
 }
 // Checks if string is a keyword or not
-int isKeyword(char *lexeme) {
-	int i;
-    for (i = 0; keywords[i] != NULL; i++) {
-        if (strcmp(keywords[i], lexeme) == 0) {
-            switch (i) {
-                case 0:
-                	return constsym;
-                case 1:
-                	return varsym;
-                case 2:
-                	return procsym;
-                case 3:
-                	return callsym;
-                case 4:
-                	return beginsym;
-                case 5:
-                	return endsym;
-                case 6:
-                	return ifsym;
-                case 7:
-                	return thensym;
-                case 8:
-                	return xorsym;
-                case 9:
-                	return elsesym;
-                case 10:
-                	return whilesym;
-                case 11:
-                	return dosym;
-                case 12:
-                	return readsym;
-                case 13:
-                	return writesym;
-                default:
-                    return 0;   // Invalid keyword
-            }
-        }
+int isKeyword(char *string) {
+    for (int i = 0; keywords[i] != NULL; i++) {
+        if (strcmp(keywords[i], string) == 0) {
+			return i;
+		}
     }
     return 0; // Not a keyword
 }
@@ -409,4 +474,23 @@ int isSpecialCase(char *string) {
 		!strcmp(string, ">="))
 		return 1;
 	return 0;
+}
+// Prints the lexeme list
+void printLexemeList() {
+    printf("\nLexeme List:\n");
+    for (int i = 0; i < lexemeCount; i++) {
+		// Checks if to print identifier or not
+		if (lexemeTable[i].type == identsym)
+			printf("%d %s ", lexemeTable[i].type, lexemeTable[i].ident);
+		else if (lexemeTable[i].type == numbersym)
+			printf("%d %d ", lexemeTable[i].type, lexemeTable[i].num);
+		else 
+			printf("%d ", lexemeTable[i].type);
+	}
+    printf("\n");
+}
+
+void printSource(char **source) {
+	for (int i = 0; i < progLen; i++)
+		printf("%s\n", source[i]);
 }
